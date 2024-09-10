50cf46c620b13ae0da96a55b7f2cdd613bff1386
giulerg <henrytheanxious@gmail.com>
2024-09-10 23:26:42 +0300

    last changes

--- calculator/main.py
+++ calculator/main.py
@@ -10,15 +10,29 @@ BUTTONS = ['CE', 'C', '<-', '/',
            '7', '8', '9', 'x',
            '4', '5', '6', '-',
            '1', '2', '3', '+',
-           '+/-', '0', ',',  '=']
+           '+/-', '0', ',', '=']
+
 
 def remove_digit(number_string):
+    """
+    This function remove last digit from number
+    :param number_string: number in (string)
+    :return: number without last digit (Decimal)
+    """
     if len(number_string) == 1:
         return Decimal(0)
     else:
         return Decimal(number_string[:-1])
 
+
 def add_digit(number, new_digit, add_point):
+    """
+    Function for adding any digit to number (or point for Decimal)
+    :param number:
+    :param new_digit:
+    :param add_point:
+    :return:
+    """
     if len(str(number)) < 16:
         if isinstance(number, int):
             if add_point:
@@ -29,7 +43,13 @@ def add_digit(number, new_digit, add_point):
             number = Decimal(str(number) + new_digit)
     return number
 
+
 class Calculator(QMainWindow):
+    """
+    Class which draw window and make
+    simple maths operations
+    """
+
     def __init__(self):
         self.firstNumber = None
         self.secondNumber = None
@@ -46,6 +66,9 @@ class Calculator(QMainWindow):
         self.draw_calculator()
 
     def draw_calculator(self):
+        """
+        Function for rendering the calculator window
+        """
         self.setWindowTitle("Calculator")
         self.setFixedSize(QSize(300, 500))
 
@@ -65,6 +88,7 @@ class Calculator(QMainWindow):
 
         self.label.setFont(font)
 
+        #Adding all buttons on window
         for i in range(len(BUTTONS)):
             button = QPushButton(f"{BUTTONS[i]}")
             button.setFixedSize(70, 50)
@@ -72,8 +96,12 @@ class Calculator(QMainWindow):
             layout.addWidget(button, (i // 4) + 1, i % 4)
 
         central_widget.setLayout(layout)
+        pass
 
     def clean_variables(self):
+        """
+        Method for cleaning all сlass fields
+        """
         self.firstNumber = None
         self.secondNumber = None
         self.result = None
@@ -83,33 +111,46 @@ class Calculator(QMainWindow):
         self.first_turn = True
         self.last_pressed_button = None
 
-    def save_number(self, text):
+    def save_number(self, symbol):
+        """
+        Function for saving digits in numbers
+        :param symbol: digit or . (string)
+        """
         if self.first_turn:
             if self.firstNumber is None:
-                self.firstNumber = int(text)
+                self.firstNumber = int(symbol)
             elif self.result is not None:
-                self.firstNumber = int(text)
+                self.firstNumber = int(symbol)
                 self.result = None
             else:
                 self.firstNumber = add_digit(
-                    self.firstNumber, text, self.add_point)
+                    self.firstNumber, symbol, self.add_point)
 
             self.label.setText(str(self.firstNumber))
         else:
             if self.secondNumber is None:
-                self.secondNumber = int(text)
+                self.secondNumber = int(symbol)
             else:
                 self.secondNumber = add_digit(
-                    self.secondNumber, text, self.add_point)
+                    self.secondNumber, symbol, self.add_point)
             self.label.setText(str(self.secondNumber))
             self.prev_operation = self.operation
 
         self.add_point = None
 
     def find_result(self, show_result):
+        """
+        Function for calculating
+        :param show_result: field which responsible
+        for showing result on screen (bool)
+        """
+        #If the second element is not provided,
+        # we will copy the first one into it.
         if self.secondNumber is None:
             self.secondNumber = self.firstNumber
 
+        #we need prev_operation if user dont click =
+        #but calculates with other operators
         if self.prev_operation is not None:
             operation = self.prev_operation
         else:
@@ -141,15 +182,19 @@ class Calculator(QMainWindow):
                 self.label.setText("{:.8e}".format(self.result))
             else:
                 self.label.setText(str(self.result))
-        # changing for more clicking =
-        self.firstNumber = self.result
 
+        # changing value for next operations
+        self.firstNumber = self.result
         self.first_turn = True
 
-    def save_operation(self, text):
-        if self.last_pressed_button != text:
-            self.label.setText(text)
-            self.operation = text
+    def save_operation(self, symbol):
+        """
+        Function for saving +,-,*,/,
+        :param symbol: chosen operation (string)
+        """
+        if self.last_pressed_button != symbol:
+            self.label.setText(symbol)
+            self.operation = symbol
             self.first_turn = False
             if self.prev_operation is None:
                 self.secondNumber = None
@@ -159,6 +204,9 @@ class Calculator(QMainWindow):
                 self.first_turn = False
 
     def backspace(self):
+        """
+        Method for removing last symbol from number
+        """
         if self.firstNumber is not None and self.first_turn:
             self.firstNumber = remove_digit(str(self.firstNumber))
             self.label.setText(str(self.firstNumber))
@@ -167,6 +215,10 @@ class Calculator(QMainWindow):
             self.label.setText(str(self.secondNumber))
 
     def keyPressEvent(self, event: QKeyEvent):
+        """
+        Method for tracking keyboard using the calculator
+        :param event:
+        """
         key = event.key()
         if Qt.Key.Key_0 <= key <= Qt.Key.Key_9:
             self.save_number(chr(key))
@@ -178,10 +230,20 @@ class Calculator(QMainWindow):
         elif key in (Qt.Key.Key_Period, Qt.Key.Key_Comma):
             self.make_number_with_point()
         elif key in (Qt.Key.Key_Plus, Qt.Key.Key_Minus, Qt.Key.Key_Asterisk,
-                     Qt.Key.Key_Slash, Qt.Key.Key_Percent):
+                     Qt.Key.Key_Slash):
             self.save_operation(chr(key))
 
+        if (key in (Qt.Key.Key_Equal, Qt.Key.Key_Period,
+                   Qt.Key.Key_Comma,Qt.Key.Key_Plus,
+                   Qt.Key.Key_Minus, Qt.Key.Key_Asterisk,
+                    Qt.Key.Key_Slash)
+                or Qt.Key.Key_0 <= key <= Qt.Key.Key_9):
+            self.last_pressed_button = chr(key)
+
     def make_number_with_point(self):
+        """
+        Method for adding . to numbers (Decimal)
+        """
         if self.first_turn:
             if self.firstNumber is None:
                 self.firstNumber = 0
@@ -198,11 +260,14 @@ class Calculator(QMainWindow):
         self.add_point = True
 
     def make_operation(self):
+        """
+        Method for tracking pressed buttons
+        """
         button = self.sender()
-        text = button.text()
+        symbol = button.text()
 
         try:
-            match text:
+            match symbol:
                 case 'CE':
                     if self.first_turn:
                         self.firstNumber = 0
@@ -230,12 +295,12 @@ class Calculator(QMainWindow):
                     self.backspace()
                 case ',':
                     self.make_number_with_point()
-                case _ if text.isnumeric():
-                    self.save_number(text)
+                case _ if symbol.isnumeric():
+                    self.save_number(symbol)
                 case _:
-                    self.save_operation(text)
+                    self.save_operation(symbol)
 
-            self.last_pressed_button = text
+            self.last_pressed_button = symbol
 
         except ZeroDivisionError:
             self.label.setText("you try divide by 0")
